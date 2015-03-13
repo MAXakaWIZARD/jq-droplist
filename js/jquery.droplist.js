b7d155245ce7b3c81633f4c7ab11d6b7ca6c9206
MAXakaWIZARD <maxakawizard@gmail.com>
2015-03-13 16:14:50 +0200

    Fix keyboard behavior

--- js/jquery.droplist.js
+++ js/jquery.droplist.js
@@ -1,10 +1,6 @@
 (function($) {
-	$.fn.droplist = function(options) {
-		var opt = $.extend({
-
-		}, options);
-
-        $(document).on('click', function(e) {
+	$.fn.droplist = function() {
+		$(document).on('click', function(e) {
             if (!$(e.target).parents().hasClass('jq-droplist-container')) {
                 $('.jq-droplist-container .jq-droplist-items').hide();
             }
@@ -17,7 +13,7 @@
                 $select.text($(this).text()).data('value', $(this).data('value'));
                 $(this).siblings('li').removeClass('current');
                 $(this).addClass('current');
-                $items.hide();
+                $ul.hide();
                 $plainSelect.val($(this).data('value'));
             }
 
@@ -35,22 +31,35 @@
             var $select = $('<div></div>').addClass('jq-droplist-select');
             $container.append($select);
 
-            var $items = $('<ul></ul>').addClass('jq-droplist-items');
+            var $ul = $('<ul></ul>').addClass('jq-droplist-items');
             $el.find('option').each(function () {
                 var $item = $('<li></li>').text($(this).text()).data('value', $(this).attr('value'));
                 $item.click($.proxy(itemClick, $item));
-                $items.append($item);
+
+                $ul.append($item);
             });
-            $items.find('li:first-child').addClass('current');
-            $container.append($items);
+
+            var $items = $ul.find('li');
+
+            $items.hover(
+                function() {
+                    $items.removeClass('selected');
+                    $(this).addClass('selected');
+                }, function() {
+                    $(this).removeClass('selected');
+                }
+            );
+
+            $items.filter(':first-child').addClass('current');
+            $container.append($ul);
 
             $container.insertBefore($el);
             $container.append($el);
 
             $select.click(function () {
-                var visible = $items.is(':visible');
+                var visible = $ul.is(':visible');
                 $('.jq-droplist-container .jq-droplist-items').hide();
-                $items.toggle(!visible);
+                $ul.toggle(!visible);
             });
 
             $plainSelect.on('focus', function() {
@@ -60,29 +69,29 @@
                 $select.removeClass('focus');
             })
             .on('keyup', function(e) {
-                var $selected = $items.find('li.selected');
-
-                console.log(e.which);
+                var $selected = $items.filter('.selected');
 
                 switch (e.which) {
                     case 40:
                         //down
-                        if ($items.is(':visible')) {
-                            $items.find('li').not($selected).removeClass('selected');
+                        e.preventDefault();
+                        if ($ul.is(':visible')) {
+                            $items.not($selected).removeClass('selected');
                             if ($selected.length === 0) {
-                                $items.find('li:first-child').addClass('selected');
+                                $items.filter(':first-child').addClass('selected');
                             } else if (!$selected.is(':last-child')) {
                                 $selected.next('li').addClass('selected');
                                 $selected.removeClass('selected');
                             }
                         } else {
-                            $items.show();
-                            $items.find('li').removeClass('selected').filter('li:first-child').addClass('selected');
+                            $ul.show();
+                            $items.removeClass('selected').filter('li:first-child').addClass('selected');
                         }
                         break;
                     case 38:
                         //up
-                        $items.find('li').not($selected).removeClass('selected');
+                        e.preventDefault();
+                        $items.not($selected).removeClass('selected');
                         if (!$selected.is(':first-child')) {
                             $selected.prev('li').addClass('selected');
                             $selected.removeClass('selected');
@@ -90,24 +99,30 @@
                         break;
                     case 13:
                         //enter
-                        if ($items.is(':visible')) {
+                        e.preventDefault();
+                        if ($ul.is(':visible')) {
                             $selected.click();
                         } else {
-                            $items.show();
-                            $items.find('li').removeClass('selected').filter('li:first-child').addClass('selected');
+                            $ul.show();
+                            $items.removeClass('selected').filter('li:first-child').addClass('selected');
                         }
                         break;
                     case 27:
                         //escape
-                        $items.hide();
+                        e.preventDefault();
+                        $ul.hide();
                         break;
                 }
             }).on('keydown', function (e) {
+                if (e.which != 9) {
+                    //do not prevent original select from receiving tab key (for focus switching)
+                    e.preventDefault();
+                }
+
                 if (e.which == 32) {
                     //space
-                    e.preventDefault();
                     $select.click();
-                    $items.find('li').removeClass('selected').filter('li:first-child').addClass('selected');
+                    $items.removeClass('selected').filter('li:first-child').addClass('selected');
                 }
             });
 		});
